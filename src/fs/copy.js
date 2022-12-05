7a366050b61cfb6313c6d58a5432b16ccb80b51b
lomserg <lomserg@gmail.com>
2022-12-05 22:32:54 +0300

    feat: refactoring

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,10 +1,11 @@
 import * as fs from 'node:fs/promises';
 import { fileURLToPath } from 'url';
+import { dirname } from 'node:path';
 const err = new Error('FS operation failed');
-const projectFolderPath = new URL('./files/', import.meta.url);
-const copyFolderPath = new URL('./files_copy/', import.meta.url)  
-const anotherFile = new URL('./files/fresh.txt', import.meta.url);
-const copyAnotherFile = new URL('./files_copy/fresh.txt', import.meta.url);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+
 
 async function checkExists(path) {  
     try {
@@ -19,21 +20,27 @@ async function checkExists(path) {
 
 
 const copy = async () => {
-    //  проверка есть ли папка files, дожно быть true
-    const folderExist = await checkExists(projectFolderPath)
-   // проверка есть уже существует папка files_copy, дожно быть false
-    const copyFolderExist = await checkExists(copyFolderPath)
-    console.log(folderExist)
-    console.log(copyFolderExist)
-   if(folderExist && !copyFolderExist) {
-        // создаваемую папку files_copy
-    await fs.mkdir(copyFolderPath, { recursive: true });
-     //копируешь содержимое files 
-    await fs.copyFile(anotherFile, copyAnotherFile);
-   } else {
+  const projectFolderPath = __dirname + '/files/'
+const copyFolderPath = __dirname + '/files_copy/'  
+const anotherFile = __dirname + '/files/fresh.txt'
+const copyAnotherFile = __dirname + '/files_copy/fresh.txt'
+   try{
+     const folderExist = await checkExists(projectFolderPath)
+     const copyFolderExist = await checkExists(copyFolderPath)
+     
+     if(folderExist && !copyFolderExist) {
+      
+      await fs.mkdir(copyFolderPath, { recursive: true });
+      
+      await fs.copyFile(anotherFile, copyAnotherFile);
+     } else{
+      throw err
+     }
+   } catch(err){
     throw err
    }
-}
+   }
+
 
 
 
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,23 +1,28 @@
-
 import * as fs from 'node:fs/promises';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const err = new Error('FS operation failed');
-const anotherFile = new URL('./files/fresh.txt', import.meta.url);
-const fileText = 'I am fresh and young'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 async function checkExists(path) {  
-    try {
-      await fs.access(path)
-      console.log('FS operation failed')
-        return true
-    } catch {
-        
-      return false
-    }
+  try {
+    await fs.access(path)
+    console.log('FS operation failed')
+    return true
+  } catch {
+    
+    return false
   }
+}
 
 
 const create = async () => {
-    // Write your code here 
+  // Write your code here 
+  
+  const anotherFile = __dirname + '/files/fresh.txt'
+  const fileText = 'I am fresh and young'
     
     const fileIsExist = await checkExists(anotherFile)
         if (fileIsExist) {
